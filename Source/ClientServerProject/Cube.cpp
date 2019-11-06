a02ae8b50856e861c93aabb4a6d4d5e13bc8ee39
Ross <rossnoltag@gmail.com>
2019-11-06 10:41:27 +0000

    Blank Canvas

---
+++ Source/ClientServerProject/Cube.cpp
@@ -0,0 +1,54 @@
+
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "Cube.h"
+
+//No arguments in the constructor 
+ACube::ACube()
+{
+	//Allow ticking....sort of...come back to
+	PrimaryActorTick.bCanEverTick = true;
+
+	//For no movment Error
+	SetMobility(EComponentMobility::Movable);
+}
+
+void ACube::BeginPlay()
+{
+	Super::BeginPlay();
+	//Setting replicates on the server so that inputs update on server and client and visa versa
+	if (HasAuthority())
+	{
+		SetReplicates(true);        // Replicate cube mesh from server on client
+		SetReplicateMovement(true); // Replicate cube mesh movment
+	}
+
+	//Start location of cube
+	GlobalStartLocation = GetActorLocation();
+
+	//Target location of cube
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void ACube::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime); //Super somthing?
+
+	//Has Authority... Is the server, with replicates set, they will update on both, from the server
+	if (HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+		float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+		if (JourneyTravelled >= JourneyLength)
+		{
+			FVector Swap = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = Swap;
+		}
+
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		Location += Speed * DeltaTime * Direction;
+		SetActorLocation(Location);
+	}
+}
---
+++ Source/ClientServerProject/Cube.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "Cube.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class CLIENTSERVERPROJECT_API ACube : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+		//Public specifier in the constructor
+public:
+	//Set up ticking
+	ACube();
+
+	/*
+	Adding the keyword �virtual� to a function header turns it
+	into a virtual function.
+
+	Virtual functions are used to make sure the child class
+	invokes the correct function.
+
+	What this means is that the compiler will no longer
+	consider the function for static linking. Instead, which
+	function gets invoke will be determined at
+	runtime(dynamic linkage).
+
+	Source code file            - C++ Proprocessor
+
+	Expanded source code file   - Compiler
+
+	Assembler file              - Assembler
+
+	Object code file			- Linker
+
+	Executable file				- .exe
+
+	override - a safety overgaurd to make sure its spelled right
+	and not creating somthing new.
+	*/
+
+	virtual void Tick(float DeltaTime)override;
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+		float Speed = 20;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+		FVector TargetLocation;
+
+private:
+	FVector GlobalStartLocation;
+	FVector GlobalTargetLocation;
+};
